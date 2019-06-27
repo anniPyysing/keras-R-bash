9ab5dddb171482faacf80e00a18c51d01ef6907c
anniPyysing <39370168+anniPyysing@users.noreply.github.com>
2019-06-27 16:17:07 +0300

    Create keras-R-bash.bash

---
+++ keras-R-bash.bash
@@ -0,0 +1,16 @@
+## Script that downloads
+## some Jupyter notebooks 
+
+cd /home/jovyan
+
+# git reflog requires a name and email if user is not in passwd
+# even if you're only cloning
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+
+git clone https://github.com/anniPyysing/keras-R.git
+
+rmdir work
+rm ml-bash-file.bash
+
+#pip install numpy --upgrade
